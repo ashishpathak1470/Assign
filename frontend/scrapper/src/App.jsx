5da3dda89fcad9a7732ffc2068957a6ceb51689e
Ashish <145586793+ashishpathak1470@users.noreply.github.com>
2024-08-06 12:31:38 +0530

    link change

--- frontend/scrapper/src/App.jsx
+++ frontend/scrapper/src/App.jsx
@@ -53,7 +53,7 @@ function App() {
     setLoading(true);
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/domain-scrape",
+        "https://assignbkend.onrender.com/api/domain-scrape",
         { domain }
       );
       setProducts(response.data);
